0d2be32ce5203621aacae7d7388d620aa330cff2
manoj singh <manojsingh00963@gmail.com>
2024-08-18 20:39:05 +0530

    'Text Utils App'

---
+++ textutils/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Component/Navbar';
+
+function App() {
+  return (
+    <>
+
+      
+      {/* <Navbar title={3} aboutText ="About TExtUtils" /> // error */}
+      {/* <Navbar title="TextUtils" aboutText ="About TExtUtils" /> */}
+      <Navbar title="TextUtils" />
+      {/* <Navbar /> */}
+    </>
+
+  );
+}
+
+export default App;
