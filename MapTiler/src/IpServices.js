355ac3b5c0e7ab0dc379d103cf071d8d83249b38
manoj singh <155624353+manojsingh00963@users.noreply.github.com>
2024-09-13 13:15:52 +0530

    Delete MapTiler directory

--- MapTiler/src/IpServices.js
+++
@@ -1,7 +0,0 @@
-// IpService.js
-export const getLocationByIP = async (ip = "") => {
-    const response = await fetch(`http://ip-api.com/json/${ip}`);
-    const data = await response.json();
-    return data;
-  };
-  
